aa562d8341425f160302784ca619f5083c81a271
Francisco Javier Lindo <flindo10@gmail.com>
2017-01-11 23:12:00 +0100

    Cambios parte mis proyectos

--- index.php
+++ index.php
@@ -422,26 +422,26 @@
 
 						<ul class="project-wrapper wow animated fadeInUp">
 							<li class="portfolio-item">
-								<img src="img/portfolio/item.jpg" class="img-responsive" alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat">
+								<img src="img/portfolio/macias.jpg" class="img-responsive" alt="Desarrollado por servitecex" >
 								<figcaption class="mask">
-									<h3>Wall street</h3>
-									<p>Lorem Ipsum is simply dummy text of the printing and typesetting ndustry. </p>
+									<h3>Electrodomesticos Macias</h3>
+									<p>Desarrollado en parte por Francisco Javier Lindo Vázquez. </p>
 								</figcaption>
 								<ul class="external">
-									<li><a class="fancybox" title="Araund The world" data-fancybox-group="works" href="img/portfolio/item.jpg"><i class="fa fa-search"></i></a></li>
-									<li><a href=""><i class="fa fa-link"></i></a></li>
+									<li><a class="fancybox" title="Electrodomesticos Macias" data-fancybox-group="works" href="img/portfolio/macias.jpg"><i class="fa fa-search"></i></a></li>
+									<li><a href="http://electrodomesticosmacias.com/"><i class="fa fa-link"></i></a></li>
 								</ul>
 							</li>
 
 							<li class="portfolio-item">
-								<img src="img/portfolio/item2.jpg" class="img-responsive" alt="Lorem Ipsum is simply dummy text of the printing and typesetting ndustry. ">
+								<img src="img/portfolio/abogados.jpg" class="img-responsive" alt="Desarrollado por servitecex">
 								<figcaption class="mask">
-									<h3>Wall street</h3>
-									<p>Lorem Ipsum is simply dummy text of the printing and typesetting ndustry. </p>
+									<h3>Abogados DaP</h3>
+									<p>Desarrollado en parte por Francisco Javier Lindo Vázquez. </p>
 								</figcaption>
 								<ul class="external">
-									<li><a class="fancybox" title="Wall street" href="img/slider/banner.jpg" data-fancybox-group="works" ><i class="fa fa-search"></i></a></li>
-									<li><a href=""><i class="fa fa-link"></i></a></li>
+									<li><a class="fancybox" title="Abogados DaP" href="img/portfolio/abogados.jpg" data-fancybox-group="works" ><i class="fa fa-search"></i></a></li>
+									<li><a href="http://abogadosdap.com"><i class="fa fa-link"></i></a></li>
 								</ul>
 							</li>
 
